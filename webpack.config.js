108138d6c3433f2140d8dffeac9d7240fe124267
matil019 <yohashi1991@gmail.com>
2022-11-06 11:47:09 +0900

    Add src/ directory

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   // mode: 'production',
   mode: 'development',
-  entry: './index.tsx',
+  entry: './src/index.tsx',
   module: {
     rules: [
       {
