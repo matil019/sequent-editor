2f145c9e082c5dd5e5e94b77241103d942c879c9
matil019 <yohashi1991@gmail.com>
2022-11-06 13:08:07 +0900

    Add click event handlers in the infer mode

--- src/sequent-infer.tsx
+++ src/sequent-infer.tsx
@@ -1,6 +1,41 @@
 import katex from 'katex';
 import React from 'react';
-import { Expr, theKatexOptions } from './common';
+import { useId } from 'react';
+import { Expr, exprToString, theKatexOptions } from './common';
+
+// TODO name
+function foo(target: HTMLElement, exprs: Expr[], idPrefix: string, handleClickWhole: (index: number) => void, handleClickRoot: (index: number) => void): void {
+  const wholeId = (index: number) => `${idPrefix}${index.toString()}whole`;
+  const rootId = (index: number) => `${idPrefix}${index.toString()}root`;
+  const katexInput = exprs
+    .map((expr, index) => {
+      if (expr.operands.length > 0) {
+        const exprWithId = {
+          ...expr,
+          me: `\\htmlId{${rootId(index)}}{${expr.me}}`,
+        };
+        return `\\htmlId{${wholeId(index)}}{${exprToString(exprWithId)}}`;
+      } else {
+        return `\\htmlId{${wholeId(index)}}{${exprToString(expr)}}`;
+      }
+    })
+    .reduce(
+      (acc, s, index) => ((index > 0) ? acc + ", " + s : s),
+      // this value is used only when the array is empty. this is intentional
+      "\\quad",
+    );
+  katex.render(katexInput, target, theKatexOptions);
+  for (let index = 0; index < exprs.length; index++) {
+    const wholeElem = document.getElementById(wholeId(index));
+    if (wholeElem) {
+      wholeElem.addEventListener("click", () => handleClickWhole(index));
+    }
+    const rootElem = document.getElementById(rootId(index));
+    if (rootElem) {
+      rootElem.addEventListener("click", () => handleClickRoot(index));
+    }
+  }
+}
 
 export type SequentInferProps = {
   lhs: Expr[],
@@ -8,13 +43,16 @@ export type SequentInferProps = {
 }
 
 export const SequentInfer = (props: SequentInferProps) => {
+  const id = useId();
   const {lhs, rhs} = props;
-  const exprsToString = (_: any) => ""; // TODO put id attributes
+  // TODO implement actual behaviors
+  const handleWhole = (index: number) => { console.log(index.toString() + " whole clicked!"); };
+  const handleRoot = (index: number) => { console.log(index.toString() + " root clicked!"); };
   return (
     <div>
-      <span ref={me => { me && katex.render(lhs.length > 0 ? exprsToString(lhs) : "\\quad", me, theKatexOptions); }} />
+      <span ref={me => { me && foo(me, lhs, id + "lhs", handleWhole, handleRoot); }} />
       <span ref={me => { me && katex.render("\\; \\vdash \\;", me, theKatexOptions); }} />
-      <span ref={me => { me && katex.render(rhs.length > 0 ? exprsToString(rhs) : "\\quad", me, theKatexOptions); }} />
+      <span ref={me => { me && foo(me, rhs, id + "rhs", handleWhole, handleRoot); }} />
     </div>
   );
 };
--- src/sequent-input.tsx
+++ src/sequent-input.tsx
@@ -2,33 +2,7 @@ import katex from 'katex';
 import React from 'react';
 import { useState } from 'react';
 
-import { Expr, theKatexOptions } from './common';
-
-function paren(s: string): string {
-  return "(" + s + ")";
-}
-
-function exprToString(expr: Expr): string {
-  // We assume that `expr.operands.length` is correct i.e. for "\\land", it's 2, for "A", it's 0, etc.
-  const n = expr.operands.length;
-  if (n === 0) {
-    return expr.me;
-  } else if (n === 1) {
-    const op0 = expr.operands[0];
-    const sop0 = exprToString(op0);
-    return expr.me + " " + (op0.precedence < expr.precedence ? paren(sop0) : sop0);
-  } else {
-    // we assume n === 2 i.e. `expr` is an infix operator
-    // we also assume that the fixity is correct i.e. if the precedences are the same, left- and right- associativities don't mix
-    const op0 = expr.operands[0];
-    const sop0 = exprToString(op0);
-    const op1 = expr.operands[1];
-    const sop1 = exprToString(op1);
-    return ((op0.precedence < expr.precedence || op0.precedence === expr.precedence && op0.associative === "right") ? paren(sop0) : sop0)
-      + " " + expr.me + " "
-      + ((op1.precedence < expr.precedence || op1.precedence === expr.precedence && op1.associative === "left") ? paren(sop1) : sop1);
-  }
-}
+import { Expr, exprToString, theKatexOptions } from './common';
 
 function exprsToString(exprs: Expr[]): string {
   return exprs
